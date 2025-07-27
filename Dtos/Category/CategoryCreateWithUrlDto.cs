3144b9c65cc7b0058a9818fd5a99084d5bed9ce8
hoanglv2611 <hoanglvhe141123@fpt.edu.vn>
2025-07-27 14:19:27 +0700

    update

---
+++ Dtos/Category/CategoryCreateWithUrlDto.cs
@@ -0,0 +1,9 @@
+namespace Sep490_Eduseen_BE.Dtos.Category
+{
+    public class CategoryCreateWithUrlDto
+    {
+        public string CategoryName { get; set; }
+        public string? Cover { get; set; }
+        public string? HoverCover { get; set; }
+    }
+} 
