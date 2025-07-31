2406b16d6b4578cf132ac8253c7043b2cfb10864
huy231102 <huydqhe172180@fpt.edu.vn>
2025-07-31 07:52:25 +0700

    [Huy2k3] Translate mail message

--- Services/AuthService.cs
+++ Services/AuthService.cs
@@ -361,8 +361,8 @@ namespace Sep490_Eduseen_BE.Services
                 var resetLink = $"http://localhost:3000/auth/reset-password?token={token}";
                 await _emailService.SendEmailAsync(
                     user.Email,
-                    "Password Reset",
-                    $"Click the link to reset your password: <a href='{resetLink}'>Reset Password</a>. This link expires in 15 minutes."
+                    "Đặt lại mật khẩu",
+                    $"Nhấn vào link để đặt lại mật khẩu: <a href='{resetLink}'>Đặt lại mật khẩu</a>. Link này sẽ hết hạn sau 15 phút."
                 );
                 return new GenericResponseDTO { Success = true, Message = "Reset link sent. Please check your email." };
             }
